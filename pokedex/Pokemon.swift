574c9bdc8451033b47a3e05ffbb5d75cb8f50f15
Alia Ziada <aliaziada@aliaas-macbook-pro.local>
2015-12-29 18:14:26 +0200

    add pokemon class

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,25 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Alia Ziada on 12/29/15.
+//  Copyright © 2015 Ntime. All rights reserved.
+//
+
+import Foundation
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    init(name: String,pokedexId: Int){
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
