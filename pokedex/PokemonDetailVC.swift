820b2174f4fe919a57cf0cb73049eae85de921ee
Alia Ziada <aliaziada@Aliaas-MacBook-Pro.local>
2015-12-30 18:21:29 +0200

    Update Pokemon Class

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -9,8 +9,20 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-    @IBOutlet weak var nameLbl: UILabel!
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
     var pokemon: Pokemon!
     override func viewDidLoad() {
         super.viewDidLoad()
